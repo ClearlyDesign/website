8b2d93e85f445ce5e0ff0150f51b7413e42dff4d
Francois Brill <f@brill.co.za>
2024-01-02 09:41:29 -0800

    Add Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
 import "@/styles/globals.css"
 import { GeistSans } from "geist/font/sans"
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App({ Component, pageProps }) {
   return (
     <main className={GeistSans.className}>
       <Component {...pageProps} />
+      <Analytics />
     </main>
   )
 }
