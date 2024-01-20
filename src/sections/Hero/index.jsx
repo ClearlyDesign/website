6283f56fc73d5caad6a428bf9d39334f7a31d32d
Francois Brill <f@brill.co.za>
2024-01-19 16:22:55 -0800

    Hero spotlight

--- src/sections/Hero/index.jsx
+++ src/sections/Hero/index.jsx
@@ -1,7 +1,14 @@
 import { Link } from "react-scroll"
 import Header from "../../components/Header"
 import { useRef, useEffect } from "react"
-import { useInView, useAnimation, motion } from "framer-motion"
+import {
+  useInView,
+  useAnimation,
+  motion,
+  useMotionTemplate,
+  useMotionValue,
+} from "framer-motion"
+import { MouseEvent } from "react"
 
 const Hero = () => {
   const ref = useRef(null)
@@ -14,8 +21,33 @@ const Hero = () => {
     }
   }, [isInView])
 
+  let mouseX = useMotionValue(0)
+  let mouseY = useMotionValue(0)
+
+  function handleMouseMove({ currentTarget, clientX, clientY }) {
+    let { left, top } = currentTarget.getBoundingClientRect()
+    mouseX.set(clientX - left)
+    mouseY.set(clientY - top)
+  }
+
   return (
-    <div className="row-wrapper bg-gradient-to-br from-indigo-950 to-gray-950" ref={ref}>
+    <div
+      className="row-wrapper bg-gradient-to-br from-indigo-950 to-gray-950 relative group"
+      ref={ref}
+      onMouseMove={handleMouseMove}
+    >
+      <motion.div
+        className="pointer-events-none absolute -inset-px rounded-xl opacity-0 transition duration-300 group-hover:opacity-100"
+        style={{
+          background: useMotionTemplate`
+            radial-gradient(
+              650px circle at ${mouseX}px ${mouseY}px,
+              rgba(99, 102, 241, 0.15),
+              transparent 80%
+            )
+          `,
+        }}
+      />
       <div className="row-inner">
         <Header />
         <div className="text-center pt-24 pb-20 md:pb-48">
@@ -68,7 +100,7 @@ const Hero = () => {
             <a
               target="_blank"
               href={process.env.NEXT_PUBLIC_BOOKING_LINK}
-              className="bg-gradient-to-tr from-green-300 to-lime-300 hover:cursor-pointer transition-all ease-in-out duration-300 rounded-full px-6 py-3 sm:px-8 sm:py-5 text-xl text-gray-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 font-medium focus-visible:outline-green-300 hover:transform hover:-translate-y-1.5 w-full sm:w-auto"
+              className="bg-gradient-to-tr from-green-300 to-lime-300 hover:cursor-pointer transition-all ease-in-out duration-300 rounded-full px-6 py-3 sm:px-8 sm:py-5 text-xl text-gray-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 font-normal focus-visible:outline-green-300 hover:transform hover:-translate-y-1.5 w-full sm:w-auto tracking-tight"
             >
               Book a Call
             </a>
@@ -79,7 +111,7 @@ const Hero = () => {
               smooth={true}
               offset={50}
               duration={500}
-              className="bg-gray-700 hover:cursor-pointer transition-all ease-in-out duration-300 rounded-full px-6 py-3 sm:px-8 sm:py-5 text-xl text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 font-medium focus-visible:outline-green-300 focus-visible:text-green-300 hover:transform hover:-translate-y-1.5 w-full sm:w-auto"
+              className="bg-gray-500/30 border border-gray-500/30 hover:cursor-pointer transition-all ease-in-out duration-300 rounded-full px-6 py-3 sm:px-8 sm:py-5 text-xl text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 font-light focus-visible:outline-green-300 focus-visible:text-green-300 hover:transform hover:-translate-y-1.5 w-full sm:w-auto tracking-tight"
             >
               View Pricing
             </Link>
