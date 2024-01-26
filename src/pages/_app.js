5409e714e4e48f16bdd004f576eba08764ce9cff
Francois Brill <f@brill.co.za>
2024-01-26 08:18:28 -0800

    Add SpeedInsights

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,16 @@
 import "@/styles/globals.css"
-// import { GeistSans } from "geist/font/sans"
 import { Inter } from "next/font/google"
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export default function App({ Component, pageProps }) {
   return (
-    // <main className={GeistSans.className}>
     <main className={inter.className}>
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights />
     </main>
   )
 }
