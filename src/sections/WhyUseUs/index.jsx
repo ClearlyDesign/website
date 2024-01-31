514c7a05b22c531d1cd62806edd3fc0df34a457c
Francois Brill <f@brill.co.za>
2024-01-31 07:56:43 -0800

    New WhyWorkWithUs sections

--- src/sections/WhyUseUs/index.jsx
+++ src/sections/WhyUseUs/index.jsx
@@ -1,33 +1,42 @@
 import { Element } from "react-scroll"
 import SectionHeader from "@/components/SectionHeader"
-import {
-  ArrowsRightLeftIcon,
-  ClockIcon,
-  CreditCardIcon,
-  CubeTransparentIcon,
-  DocumentCheckIcon,
-  DocumentTextIcon,
-  NoSymbolIcon,
-  PaintBrushIcon,
-  PencilIcon,
-  SwatchIcon,
-  UserGroupIcon,
-} from "@heroicons/react/24/outline"
 import clsx from "clsx"
+import {
+  DocumentCheckIcon,
+  SwatchIcon,
+  ChatBubbleLeftRightIcon,
+  IdentificationIcon,
+  PlayPauseIcon,
+  ClockIcon,
+  UsersIcon,
+  PencilIcon,
+  CreditCardIcon,
+  PencilSquareIcon,
+} from "@heroicons/react/24/solid"
+import {
+  CubeTransparentIcon,
+  NoSymbolIcon,
+  ArrowsRightLeftIcon,
+  ClipboardDocumentCheckIcon,
+  CursorArrowRaysIcon,
+  RocketLaunchIcon,
+  SparklesIcon,
+} from "@heroicons/react/16/solid"
+import Image from "next/image"
 
 const WhyUseUs = () => {
   return (
     <Element name="whyUseUs">
       <section>
-        <div className="row-wrapper row-y-spacing">
+        <div className="row-wrapper row-t-spacing">
           <div className="row-inner">
             <SectionHeader
               title={`Great design is hard. <span class="text-green-500">We make it easy.</span>`}
               description="We&#39;ve spent decades making it this simple. You&#39;ll never want to go back to random freelancers, expensive agencies, or junior internal designers."
             />
-            <div className="mt-10 grid grid-cols-12 gap-4">
-              {items.map((item, i) => (
-                <Item item={item} key={item.title} i={i} />
+            <div className="">
+              {rows.map((row, i) => (
+                <Row row={row} key={i} />
               ))}
             </div>
           </div>
@@ -38,87 +47,168 @@ const WhyUseUs = () => {
 }
 export default WhyUseUs
 
-const Item = ({ item, i }) => {
+const Row = ({ row }) => {
+  const { eyebrow, title, description, benefits, styles, illustration } = row
   return (
-    <div
-      className={clsx(
-        "bg-gray-50 border border-gray-200 p-4 sm:p-6 rounded-2xl",
-        i < 3
-          ? "col-span-12 sm:col-span-6 md:col-span-4"
-          : "col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3",
-      )}
-    >
-      <div className="inline-flex items-center justify-center p-3 mb-2 bg-gradient-to-tr from-green-300 to-lime-300 rounded-full">
-        <item.icon className="h-6 w-6 shrink-0 text-green-800" aria-hidden="true" />
+    <>
+      <div className="group pt-10 border border-gray-200 rounded-2xl bg-gradient-to-b from-gray-50 via-gray-100 to-gray-100">
+        <div className="flex justify-center">
+          <div
+            className={clsx(
+              "text-sm uppercase text-gray-400 inline-flex items-center gap-2 border border-gray-300 rounded-full py-2 pl-3.5 pr-4 ani",
+              styles.eyebrow,
+            )}
+          >
+            <eyebrow.icon className="w-4 h-4" aria-hidden={true} />
+            {eyebrow.title}
+          </div>
+        </div>
+        <div className="text-center px-10 mt-5 mb-10 space-y-5 max-w-4xl">
+          <h2 className="text-4xl text-gray-800">{title}</h2>
+          <p
+            className="text-lg text-gray-500"
+            dangerouslySetInnerHTML={{ __html: description }}
+          />
+        </div>
+        <Image src={illustration} className="" width={1220} height={188} alt="" />
       </div>
-      <h5 className="text-gray-900 mt-1">{item.title}</h5>
-      <p className="text-gray-600">{item.description}</p>
-    </div>
+      <div className="mt-10 grid  grid-cols-1  sm:grid-cols-2 lg:grid-cols-4 gap-10 mb-36">
+        {benefits.map((benefit) => (
+          <div key={benefit.title}>
+            <p className="font-bold text-[20px] tracking-tighter inline-flex">
+              <benefit.icon className={clsx("w-6 h-6 mr-2 shrink-0", styles.icon)} />
+              {benefit.title}
+            </p>
+            <p
+              className="text-gray-500 mt-2"
+              dangerouslySetInnerHTML={{ __html: benefit.description }}
+            />
+          </div>
+        ))}
+      </div>
+    </>
   )
 }
-const items = [
+
+const rows = [
   {
-    title: "Premium Quality Design",
-    description: "Beautiful, functional and highly detailed designs at your fingertips.",
-    icon: SwatchIcon,
-  },
-  {
-    title: "Unique and all yours",
+    eyebrow: {
+      title: "Creative Superpower",
+      icon: SparklesIcon,
+    },
+    title: "Elevate Your Product with Cutting-Edge Design",
     description:
-      "We craft each of your designs from scratch for you and it is 100% yours to keep forever.",
-    icon: PaintBrushIcon,
+      "At the heart of your product lies the potential for a distinctive design that not only sets you apart but becomes your signature. We specialize in crafting bespoke designs that&#39;s unique and timeless.",
+    illustration: "/images/illustration-benefit-craft.svg",
+    benefits: [
+      {
+        title: "Made Just For You",
+        description:
+          "Leading with what your user&#39;s needs, we focus on best practice to design just what they desire.",
+        icon: ChatBubbleLeftRightIcon,
+      },
+      {
+        title: "High Quality Craft",
+        description:
+          "We sweat all the details. Highly functional and detailed designs at your fingertips.",
+        icon: SwatchIcon,
+      },
+      {
+        title: "Only Senior Talent",
+        description:
+          "Get access to world-class design talent with decades of experience to look after each detail.",
+        icon: IdentificationIcon,
+      },
+      {
+        title: "Unlimited Revisions",
+        description:
+          "It&#39s made just for you, so we&#39ll keep making changes until you&#39re 100% satisfied.",
+        icon: DocumentCheckIcon,
+      },
+    ],
+    styles: {
+      icon: "text-green-500",
+      eyebrow: "group-hover:text-green-600 group-hover:border-green-600",
+    },
   },
   {
-    title: "Unlimited Revisions",
+    eyebrow: {
+      title: "Organize to focus",
+      icon: ClipboardDocumentCheckIcon,
+    },
+    title: "Seamless Workflow for Design On Demand",
     description:
-      "It's made for you, so we'll keep making changes until you're 100% satisfied.",
-    icon: DocumentCheckIcon,
+      "Our process is streamlined for efficiency, centred around clear communication. Experience the ease of a design service that adapts to your pace, ensuring your creative needs are met with precision and promptness.",
+    illustration: "/images/illustration-benefit-workflow.svg",
+    benefits: [
+      {
+        title: "Stay In Control",
+        description:
+          "Plan projects, submit design requests, organize priority and view active or completed tasks with ease.",
+        icon: PencilSquareIcon,
+      },
+      {
+        title: "Delivery Matters To Us",
+        description:
+          "We get right on it. You&#39;ll start seeing progress within a few business days on average, Monday to Friday.",
+        icon: ClockIcon,
+      },
+      {
+        title: "Async Or Integrated",
+        description:
+          "Keep us in the background or ingrained with your team for real-time collaboration.",
+        icon: ArrowsRightLeftIcon,
+      },
+      {
+        title: "Add Your Team",
+        description:
+          "Invite your team, anyone can submit requests and track design projects or task progress.",
+        icon: UsersIcon,
+      },
+    ],
+    styles: {
+      icon: "text-indigo-500",
+      eyebrow: "group-hover:text-indigo-600 group-hover:border-indigo-600",
+    },
   },
   {
-    title: "Affordable Pricing",
+    eyebrow: {
+      title: "straight to work",
+      icon: CursorArrowRaysIcon,
+    },
+    title: "We Make Working With Us As Simple As Possible",
     description:
-      "Get access to world-class design talent with decades of experience for one flat monthly fee.",
-    icon: CreditCardIcon,
-  },
-  {
-    title: "Payments Your Way",
-    description:
-      "Easy credit card payments or invoice based terms, which ever works best for your team.",
-    icon: DocumentTextIcon,
-  },
-  {
-    title: "No Contracts",
-    description:
-      "We don't do SOW's, long-terms agreements, or any other documents to drag out the process.",
-    icon: NoSymbolIcon,
-  },
-  {
-    title: "Flexible and Scalable",
-    description: "Scale up or down based on your needs. You can even cancel at anytime.",
-    icon: CubeTransparentIcon,
-  },
-  {
-    title: "Async & Easy",
-    description:
-      "We leverage tools to better communicate with no meetings. Saving time and delivering results.",
-    icon: ArrowsRightLeftIcon,
-  },
-  {
-    title: "Delivery Matters to us",
-    description:
-      "You'll receive your design within a few business days on average, Monday to Friday.",
-    icon: ClockIcon,
-  },
-  {
-    title: "Add Team Members",
-    description:
-      "Invite your entire team, so anyone can submit requests and track design task progress.",
-    icon: UserGroupIcon,
-  },
-  {
-    title: "Stay in Control",
-    description:
-      "Submit design requests, reorganize priority and view active and completed tasks with ease.",
-    icon: PencilIcon,
+      "We&#39;re looking for partners that want to work with us. So no long-term commitments or agreements from us. We want you to continuously get value and amazing designs so we can grow together.",
+    illustration: "/images/illustration-benefit-admin.svg",
+    benefits: [
+      {
+        title: "Payments Your Way",
+        description:
+          "Easy credit card payments or invoice based terms, whichever works best for your team.",
+        icon: CreditCardIcon,
+      },
+      {
+        title: "No Contracts",
+        description:
+          "We don&#39;t do SOW&#39;s, long-terms agreements, or any other documents to drag out the process.",
+        icon: NoSymbolIcon,
+      },
+      {
+        title: "Flexible and Scalable",
+        description:
+          "Scale up or down based on your needs. Get the added firing power with bigger projects and tighter deadlines.",
+        icon: CubeTransparentIcon,
+      },
+      {
+        title: "Pause or Cancel Anytime",
+        description:
+          "Burned through your design backlog? Great! You know where to find us when the backlog fills up again.",
+        icon: PlayPauseIcon,
+      },
+    ],
+    styles: {
+      icon: "text-lime-500",
+      eyebrow: "group-hover:text-lime-600 group-hover:border-lime-600",
+    },
   },
 ]
