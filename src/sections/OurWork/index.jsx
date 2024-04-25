ee713aadd534e24235012e8615e957157d0ccc8c
Francois Brill <f@brill.co.za>
2024-04-25 12:16:51 -0700

    Tracking Links

--- src/sections/OurWork/index.jsx
+++ src/sections/OurWork/index.jsx
@@ -24,7 +24,7 @@ const OurWork = () => {
             <div className="space-y-5 pt-5 md:pt-10 lg:pt-20 hidden md:block">
               <AniCardFlip>
                 <a
-                  href="https://www.figma.com/proto/1p8Zi5oavqbqFXy9CEk7LC/Portfolio---Clearly-Design?type=design&node-id=41-143&viewport=2247%2C497%2C0.73&t=DzmlYnm0jOBsC2VD-0&scaling=scale-down-width&starting-point-node-id=31%3A7"
+                  href="https://dub.sh/clearly-p-flexi"
                   target="_blank"
                   className="bg-gray-200 overflow-hidden rounded-2xl shadow-xl aspect-square block hover:transform hover:scale-[102%] duration-500 transition-all ease-in-out"
                 >
@@ -39,7 +39,7 @@ const OurWork = () => {
               </AniCardFlip>
               <AniCardFlip>
                 <a
-                  href="https://www.figma.com/proto/1p8Zi5oavqbqFXy9CEk7LC/Portfolio---Clearly-Design?type=design&node-id=41-143&viewport=2247%2C497%2C0.73&t=DzmlYnm0jOBsC2VD-0&scaling=scale-down-width&starting-point-node-id=31%3A7"
+                  href="https://dub.sh/clearly-p-flexi"
                   target="_blank"
                   className="bg-gray-200 overflow-hidden rounded-2xl shadow-xl aspect-video block hover:transform hover:scale-[102%] duration-500 transition-all ease-in-out"
                 >
@@ -56,7 +56,7 @@ const OurWork = () => {
             <div className="space-y-5">
               <AniCardFlip>
                 <a
-                  href="https://www.figma.com/proto/1p8Zi5oavqbqFXy9CEk7LC/Portfolio---Clearly-Design?type=design&node-id=1-2&viewport=2247%2C497%2C0.73&t=DzmlYnm0jOBsC2VD-0&scaling=scale-down-width&starting-point-node-id=31%3A7"
+                  href="https://dub.sh/clearly-p-castos"
                   target="_blank"
                   className="bg-gray-200 overflow-hidden rounded-2xl shadow-xl aspect-video block hover:transform hover:scale-[102%] duration-500 transition-all ease-in-out"
                 >
@@ -71,7 +71,7 @@ const OurWork = () => {
               </AniCardFlip>
               <AniCardFlip>
                 <a
-                  href="https://www.figma.com/proto/1p8Zi5oavqbqFXy9CEk7LC/Portfolio---Clearly-Design?type=design&node-id=1-2&viewport=2247%2C497%2C0.73&t=DzmlYnm0jOBsC2VD-0&scaling=scale-down-width&starting-point-node-id=31%3A7"
+                  href="https://dub.sh/clearly-p-castos"
                   target="_blank"
                   className="bg-gray-200 overflow-hidden rounded-2xl shadow-xl aspect-square block hover:transform hover:scale-[102%] duration-500 transition-all ease-in-out"
                 >
@@ -88,7 +88,7 @@ const OurWork = () => {
             <div className="space-y-5 pt-5 md:pt-10 lg:pt-20">
               <AniCardFlip>
                 <a
-                  href="https://www.figma.com/proto/1p8Zi5oavqbqFXy9CEk7LC/Portfolio---Clearly-Design?type=design&node-id=41-101&viewport=2247%2C497%2C0.73&t=DzmlYnm0jOBsC2VD-0&scaling=scale-down-width&starting-point-node-id=31%3A7"
+                  href="https://dub.sh/clearly-p-wstack"
                   target="_blank"
                   className="bg-gray-200 overflow-hidden rounded-2xl shadow-xl aspect-square block hover:transform hover:scale-[102%] duration-500 transition-all ease-in-out"
                 >
@@ -103,7 +103,7 @@ const OurWork = () => {
               </AniCardFlip>
               <AniCardFlip>
                 <a
-                  href="https://www.figma.com/proto/1p8Zi5oavqbqFXy9CEk7LC/Portfolio---Clearly-Design?type=design&node-id=41-101&viewport=2247%2C497%2C0.73&t=DzmlYnm0jOBsC2VD-0&scaling=scale-down-width&starting-point-node-id=31%3A7"
+                  href="https://dub.sh/clearly-p-wstack"
                   target="_blank"
                   className="bg-gray-200 overflow-hidden rounded-2xl shadow-xl aspect-video block hover:transform hover:scale-[102%] duration-500 transition-all ease-in-out"
                 >
@@ -120,7 +120,7 @@ const OurWork = () => {
             <div className="space-y-5 hidden lg:block">
               <AniCardFlip>
                 <a
-                  href="https://www.figma.com/proto/1p8Zi5oavqbqFXy9CEk7LC/Portfolio---Clearly-Design?type=design&node-id=41-126&viewport=2247%2C497%2C0.73&t=DzmlYnm0jOBsC2VD-0&scaling=scale-down-width&starting-point-node-id=31%3A7"
+                  href="https://dub.sh/clearly-p-nodcards"
                   target="_blank"
                   className="bg-gray-200 overflow-hidden rounded-2xl shadow-xl aspect-video block hover:transform hover:scale-[102%] duration-500 transition-all ease-in-out"
                 >
@@ -135,7 +135,7 @@ const OurWork = () => {
               </AniCardFlip>
               <AniCardFlip>
                 <a
-                  href="https://www.figma.com/proto/1p8Zi5oavqbqFXy9CEk7LC/Portfolio---Clearly-Design?type=design&node-id=41-126&viewport=2247%2C497%2C0.73&t=DzmlYnm0jOBsC2VD-0&scaling=scale-down-width&starting-point-node-id=31%3A7"
+                  href="https://dub.sh/clearly-p-nodcards"
                   target="_blank"
                   className="bg-gray-200 overflow-hidden rounded-2xl shadow-xl aspect-square block hover:transform hover:scale-[102%] duration-500 transition-all ease-in-out"
                 >
@@ -153,7 +153,7 @@ const OurWork = () => {
           <div className="mt-20 text-center px-5">
             <a
               target="_blank"
-              href="https://www.figma.com/proto/1p8Zi5oavqbqFXy9CEk7LC/Portfolio---Clearly-Design?type=design&node-id=31-7&viewport=2247%2C497%2C0.73&t=DzmlYnm0jOBsC2VD-0&scaling=scale-down-width&starting-point-node-id=31%3A7"
+              href="https://dub.sh/clearly-portfolio"
               className="bg-gradient-to-r from-indigo-950 to-gray-950 hover:cursor-pointer transition-all ease-in-out duration-300 rounded-full px-6 py-3 sm:px-8 sm:py-4 text-xl text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 font-medium focus-visible:outline-gray-500 hover:transform hover:-translate-y-1.5 inline-flex items-center w-full justify-center sm:w-auto mb-2"
             >
               View Portfolio <ArrowLongRightIcon className="w-6 h-6 text-lime-400 ml-3" />
